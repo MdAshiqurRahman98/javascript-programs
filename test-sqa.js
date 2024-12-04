06fbc27a543273f423b0dc516e85d116ffcc764f
MdAshiqurRahman98 <MDASHIQUROFFICIAL@gmail.com>
2024-12-04 11:45:15 +0600

    JS program added

---
+++ test-sqa.js
@@ -0,0 +1,19 @@
+// Finding Duplicates Number
+
+function findDuplicates(arr) {
+    const frequency = {};
+    const duplicates = [];
+    for (let num of arr) {
+        frequency[num] = (frequency[num] || 0) + 1;
+    }
+
+    for (let num in frequency) {
+        if (frequency[num] > 1) {
+            duplicates.push(Number(num));
+        }
+    }
+    return duplicates;
+}
+
+const array = [1, 33, 4, 8, 1, 8, 8, 90];
+console.log(findDuplicates(array));
